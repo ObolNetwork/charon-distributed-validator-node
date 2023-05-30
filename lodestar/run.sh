7c3778b95a12043a3940de19337ad947de9afc1f
Dhruv Bodani <dhruvbodani2510@gmail.com>
2023-05-30 15:17:52 +0530

    Add lodestar VC (#187)
    Replaces teku VC with lodestar VC in the primary docker-compose.yml as
    lodestar now supports aggregations with charon.

---
+++ lodestar/run.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+for f in /home/charon/validator_keys/keystore-*.json; do
+    echo "Importing key ${f}"
+
+    # Import keystore with password.
+    node /usr/app/packages/cli/bin/lodestar validator import \
+        --dataDir="/opt/data" \
+        --network="$NETWORK" \
+        --importKeystores="$f" \
+        --importKeystoresPassword="${f//json/txt}"
+done
+
+echo "Imported all keys"
+
+node /usr/app/packages/cli/bin/lodestar validator \
+    --dataDir="/opt/data" \
+    --network="$NETWORK" \
+    --metrics=true \
+    --metrics.address="0.0.0.0" \
+    --metrics.port=5064 \
+    --beaconNodes="$BEACON_NODE_ADDRESS" \
+    --distributed
