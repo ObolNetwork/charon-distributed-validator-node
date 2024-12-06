623158a757b8c05608ca1c3c6bf1eba3b124d3e2
Kaloyan Tanev <kalo@obol.tech>
2024-12-06 15:23:58 +0200

    Add more help messages in base.sh

--- multi_cluster/base.sh
+++ multi_cluster/base.sh
@@ -3,7 +3,7 @@
 usage() {
  echo "Usage: $0 [OPTIONS] COMMAND"
  echo ""
- echo "    Manage the base ethereum node docker containers (EL, CL, MEV boost, Grafana), without interfering with any validator."
+ echo "    Manage the base ethereum node (EL, CL, MEV boost, Grafana), without interfering with any validator."
  echo ""
  echo "Commands:"
  echo "    start   Start an ethereum node, MEV-boost and Grafana."
@@ -11,6 +11,30 @@ usage() {
  echo ""
  echo "Options:"
  echo "    -h          Display this help message."
+}
+
+usage_start() {
+ echo "Usage: $0 start [OPTIONS]"
+ echo ""
+ echo "    Start the base ethereum node."
+ echo ""
+ echo "Options:"
+ echo "    -h          Display this help message."
+ echo ""
+ echo "Example:"
+ echo "  $0 start"
+}
+
+usage_stop() {
+ echo "Usage: $0 stop [OPTIONS]"
+ echo ""
+ echo "    Stop the base ethereum node."
+ echo ""
+ echo "Options:"
+ echo "    -h          Display this help message."
+ echo ""
+ echo "Example:"
+ echo "  $0 stop"
 }
 
 start() {
@@ -44,9 +68,35 @@ subcommand=$1; shift
 case "$subcommand" in
   # Parse options to the install sub command
   start)
+    while getopts ":h" opt; do
+      case $opt in
+        h)
+          usage_start
+          exit 0
+        ;;
+        ? ) # Invalid option
+          usage_start
+          exit 1
+        ;;
+      esac
+    done
+    shift $((OPTIND-1))
     start
     ;;
   stop)
+    while getopts ":h" opt; do
+      case $opt in
+        h)
+          usage_stop
+          exit 0
+        ;;
+        ? ) # Invalid option
+          usage_stop
+          exit 1
+        ;;
+      esac
+    done
+    shift $((OPTIND-1))
     stop
     ;;
   * )
