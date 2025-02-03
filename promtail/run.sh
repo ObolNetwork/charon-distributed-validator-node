269ef1645718d8f70b3a3f15716b72af95c43e76
Anthony <pham.anthony1@gmail.com>
2025-02-03 16:51:05 +0100

    fix: use en vars to create promtail conf file

--- promtail/run.sh
+++ promtail/run.sh
@@ -8,6 +8,12 @@ fi
 sed "s|\$CHARON_LOKI_ADDRESSES|${CHARON_LOKI_ADDRESSES}|g" \
     /etc/promtail/config.yml.example > /etc/promtail/config.yml
 
+sed "s|\$CLUSTER_NAME|${CLUSTER_NAME}|g" \
+    /etc/promtail/config.yml.example > /etc/promtail/config.yml
+
+sed "s|\$CLUSTER_PEER|${CLUSTER_PEER}|g" \
+    /etc/promtail/config.yml.example > /etc/promtail/config.yml
+
 # Start Promtail with the generated config
 /usr/bin/promtail \
   -config.file=/etc/promtail/config.yml
